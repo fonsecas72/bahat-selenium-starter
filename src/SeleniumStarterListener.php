e5d12b1c7e3c0207d01041ec8562eca8b30acae4
Hugo Fonseca <hugo.fonseca@beubi.com>
2015-08-10 13:42:26 +0100

    minimum working

--- src/SeleniumStarterListener.php
+++ src/SeleniumStarterListener.php
@@ -3,48 +3,27 @@
 namespace Fonsecas72\SeleniumStarterExtension;
 
 use Behat\Testwork\EventDispatcher\Event\BeforeExerciseCompleted;
-use BeubiQA\Application\Selenium\Options\SeleniumStartOptions;
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\ExecutableFinder;
-use BeubiQA\Application\Lib\ResponseWaitter;
-use GuzzleHttp\Client;
-use BeubiQA\Application\Selenium\SeleniumStarter;
-use BeubiQA\Application\Selenium\SeleniumStopper;
-use BeubiQA\Application\Selenium\SeleniumDownloader;
-use BeubiQA\Application\Lib\LogWatcher;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use BeubiQA\Application\Selenium\SeleniumHandler;
-use BeubiQA\Application\Selenium\Options\SeleniumStopOptions;
-use BeubiQA\Application\Selenium\Options\SeleniumDownloaderOptions;
 
 class SeleniumStarterListener implements EventSubscriberInterface
 {
+    /**  @var SeleniumHandler */
+    private $seleniumHandler;
+
+    public function __construct(SeleniumHandler $seleniumHandler)
+    {
+        $this->seleniumHandler = $seleniumHandler;
+    }
     public static function getSubscribedEvents()
     {
         return array(
-            BeforeExerciseCompleted::BEFORE => array('startSelenium', 10),
+            BeforeExerciseCompleted::BEFORE => array('startSelenium', 10)
         );
     }
 
-    public function startSelenium(BeforeExerciseCompleted $event)
+    public function startSelenium($event)
     {
-        $process = new Process('');
-        $exeFinder = new ExecutableFinder();
-        $httpClient = new Client();
-        $waiter = new ResponseWaitter($httpClient);
-        $seleniumStarterOptions = new SeleniumStartOptions();
-        $seleniumStarterOptions->enabledXvfb();
-        $seleniumStarter = new SeleniumStarter($seleniumStarterOptions, $process, $waiter, $exeFinder);
-        $seleniumStopperOptions = new SeleniumStopOptions();
-        $seleniumStopper = new SeleniumStopper($seleniumStopperOptions, $waiter, $httpClient);
-        $seleniumDownloaderOptions = new SeleniumDownloaderOptions();
-        $seleniumDownloader = new SeleniumDownloader($seleniumDownloaderOptions, $httpClient);
-        $seleniumLogWatcher = new LogWatcher();
-        $seleniumHandler = new SeleniumHandler(
-            $seleniumStarter,
-            $seleniumStopper,
-            $seleniumDownloader,
-            $seleniumLogWatcher
-        );
-        $seleniumHandler->start();
+        $this->seleniumHandler->start();
     }
 }
--- src/ServiceContainer/SeleniumStarterExtension.php
+++ src/ServiceContainer/SeleniumStarterExtension.php
@@ -8,23 +8,67 @@ use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Behat\Testwork\ServiceContainer\Extension;
+use BeubiQA\Application\Selenium\Options\SeleniumStartOptions;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ExecutableFinder;
+use BeubiQA\Application\Lib\ResponseWaitter;
+use GuzzleHttp\Client;
+use BeubiQA\Application\Selenium\SeleniumStarter;
+use BeubiQA\Application\Selenium\SeleniumStopper;
+use BeubiQA\Application\Selenium\SeleniumDownloader;
+use BeubiQA\Application\Lib\LogWatcher;
+use BeubiQA\Application\Selenium\SeleniumHandler;
+use BeubiQA\Application\Selenium\Options\SeleniumStopOptions;
+use BeubiQA\Application\Selenium\Options\SeleniumDownloaderOptions;
 
 class SeleniumStarterExtension implements Extension
 {
-    public function initialize(ExtensionManager $extensionManager){}
-    
+
+    public function initialize(ExtensionManager $extensionManager)
+    {
+
+    }
+
     public function load(ContainerBuilder $container, array $config)
     {
-        $definition = new Definition('Fonsecas72\SeleniumStarterExtension\SeleniumStarterListener');
+        $process = new Process('');
+        $exeFinder = new ExecutableFinder();
+        $httpClient = new Client();
+        $waiter = new ResponseWaitter($httpClient);
+        $seleniumStarterOptions = new SeleniumStartOptions();
+        $seleniumStarterOptions->enabledXvfb();
+        $seleniumStarter = new SeleniumStarter($seleniumStarterOptions, $process, $waiter, $exeFinder);
+        $seleniumStopperOptions = new SeleniumStopOptions();
+        $seleniumStopper = new SeleniumStopper($seleniumStopperOptions, $waiter, $httpClient);
+        $seleniumDownloaderOptions = new SeleniumDownloaderOptions();
+        $seleniumDownloader = new SeleniumDownloader($seleniumDownloaderOptions, $httpClient);
+        $seleniumLogWatcher = new LogWatcher();
+        $seleniumHandler = new SeleniumHandler(
+            $seleniumStarter, $seleniumStopper, $seleniumDownloader, $seleniumLogWatcher
+        );
+
+        $definition = new Definition('Fonsecas72\SeleniumStarterExtension\SeleniumStarterListener', [$seleniumHandler]);
         $definition->addTag(EventDispatcherExtension::SUBSCRIBER_TAG, array('priority' => 0));
-        $container->setDefinition('behat.selenium-starter', $definition);
+        $container->setDefinition('selenium_starter', $definition);
+
+
+
+
+
     }
 
     public function getConfigKey()
     {
-        return 'selenium-starter';
+        return 'selenium_starter';
     }
 
-    public function configure(ArrayNodeDefinition $builder){}
-    public function process(ContainerBuilder $container){}
+    public function configure(ArrayNodeDefinition $builder)
+    {
+
+    }
+
+    public function process(ContainerBuilder $container)
+    {
+
+    }
 }
